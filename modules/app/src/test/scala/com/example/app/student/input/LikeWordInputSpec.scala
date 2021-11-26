b89d7a160ee5dc61e30216bbd2ab369b6c5722fa
takafumi-s <takafumi.s.dev@gmail.com>
2021-11-26 18:45:30 +0900

    Add specs

---
+++ modules/app/src/test/scala/com/example/app/student/input/LikeWordInputSpec.scala
@@ -0,0 +1,28 @@
+package com.example.app.student.input
+
+import eu.timepit.refined.auto._
+import cats._
+
+class LikeWordInputSpec extends munit.FunSuite {
+  test("`select` is Only one `LikeWordInput` should be selected.") {
+    val pm1: LikeWordInput = LikeWordInput.StudentName("a")
+    val pm2: LikeWordInput = LikeWordInput.ClassroomName("b")
+
+    val seq1: Seq[Option[Id[LikeWordInput]]] = Seq(
+      None,
+      Some(pm1),
+      Some(pm2),
+      None
+    )
+    assertEquals(LikeWordInput.select(seq1), Some(pm1))
+
+    val seq2: Seq[Option[Id[LikeWordInput]]] = Seq(
+      None,
+      None,
+      None,
+      None
+    )
+    assertEquals(LikeWordInput.select(seq2), None)
+
+  }
+}
---
+++ modules/entity/src/test/scala/com/example/entity/classroom/ClassroomSpec.scala
@@ -0,0 +1,39 @@
+package com.example.entity.classroom
+
+import eu.timepit.refined.auto._
+import com.example.types.{ClassroomId, ClassroomName}
+import com.example.entity.ContainingWordFilter
+
+class ClassroomSpec extends munit.FunSuite {
+  test(
+    "`CList.filterByIntermediateMatch` should be filtered out when there is a partial match keyword"
+  ) {
+
+    import Classroom.CList
+
+    val c1 = Classroom(ClassroomId(1), ClassroomName("abcd"))
+    val c2 = Classroom(ClassroomId(1), ClassroomName("defg"))
+    val m1 = Some(ContainingWordFilter.ClassroomName("cd"))
+
+    val expected = List(c1)
+    val obtained = CList(List(c1, c2)).filterByIntermediateMatch(m1)
+
+    assertEquals(obtained, expected)
+  }
+
+  test(
+    "`CList.filterByIntermediateMatch` should do nothing when the `ContainingWordFilter` is `StudentName`."
+  ) {
+
+    import Classroom.CList
+
+    val c1                  = Classroom(ClassroomId(1), ClassroomName("abcd"))
+    val c2                  = Classroom(ClassroomId(1), ClassroomName("defg"))
+    val filterOfStudentName = Some(ContainingWordFilter.StudentName("cd"))
+
+    val expected = List(c1, c2)
+    val obtained = CList(List(c1, c2)).filterByIntermediateMatch(filterOfStudentName)
+
+    assertEquals(obtained, expected)
+  }
+}
